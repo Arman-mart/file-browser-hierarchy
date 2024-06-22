5eaf6074dbb92de489641732993dcdd52507602a
yourusername <your@email.com>
2024-06-22 14:59:52 +0400

    setup initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+// src/App.tsx
+
+import React, { Component } from 'react';
+
+interface AppProps {}
+
+interface AppState {}
+
+class App extends Component<AppProps, AppState> {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
